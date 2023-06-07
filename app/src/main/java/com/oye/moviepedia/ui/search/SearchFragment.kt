e2060b993f134cae9b2ef0f99bea78e1eea236b3
Ancelotow <ancelotow@gmail.com>
2023-06-07 20:40:15 +0200

    [US-2] Display search field

--- app/src/main/java/com/oye/moviepedia/ui/search/SearchFragment.kt
+++ app/src/main/java/com/oye/moviepedia/ui/search/SearchFragment.kt
@@ -1,44 +1,47 @@
 package com.oye.moviepedia.ui.search
 
-import android.graphics.Shader
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.oye.moviepedia.R
 
 class SearchFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -54,11 +57,20 @@ class SearchFragment : Fragment() {
 
     @Composable
     fun Body() {
-        Box(
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Title()
+        Column {
+            Box(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Title()
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxWidth().padding(top = 10.dp)
+            ) {
+                SearchTextField()
+            }
         }
+
     }
 
     @OptIn(ExperimentalTextApi::class)
@@ -76,6 +88,43 @@ class SearchFragment : Fragment() {
                     tileMode = TileMode.Clamp
                 )
             ),
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SearchTextField() {
+        var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+        TextField(
+            value = textFieldValue,
+            onValueChange = { newValue -> textFieldValue = newValue },
+            shape = RoundedCornerShape(5.dp),
+            label = {
+                Text(stringResource(id = R.string.search_hint))
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .padding(horizontal = 10.dp),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Start,
+            ),
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Search icon",
+                    tint = Color(0xFFD4D4D4),
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0x66989898),
+                textColor = Color(0xFFD4D4D4),
+                placeholderColor = Color(0xFFD4D4D4),
+                focusedIndicatorColor = Color.Transparent,
+                focusedLabelColor = Color(0xFF51B1DF),
+                unfocusedLabelColor = Color(0xFFD4D4D4),
+            )
         )
     }
 
