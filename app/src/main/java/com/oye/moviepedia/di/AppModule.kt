e782536fe9b2cfdf444ba37b483564d24a89be7d
Ancelotow <ancelotow@gmail.com>
2023-07-14 14:44:42 +0200

    Provides "Api Service" in AppModule

--- app/src/main/java/com/oye/moviepedia/di/AppModule.kt
+++ app/src/main/java/com/oye/moviepedia/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.oye.moviepedia.di
 
+import com.google.gson.GsonBuilder
+import com.oye.moviepedia.common.API_TOKEN
+import com.oye.moviepedia.common.BASE_URL
+import com.oye.moviepedia.common.LocalDateDeserializer
 import com.oye.moviepedia.data.data_sources.AuthDataSource
 import com.oye.moviepedia.data.data_sources.LogoutDataSource
 import com.oye.moviepedia.data.data_sources.MovieDataSource
@@ -9,6 +13,7 @@ import com.oye.moviepedia.data.data_sources.remote.RemoteMovieDataSource
 import com.oye.moviepedia.data.repositories.RemoteAuthRepository
 import com.oye.moviepedia.data.repositories.RemoteLogoutRepository
 import com.oye.moviepedia.data.repositories.RemoteMovieRepository
+import com.oye.moviepedia.data.services.ApiService
 import com.oye.moviepedia.domain.repositories.AuthRepository
 import com.oye.moviepedia.domain.repositories.LogoutRepository
 import com.oye.moviepedia.domain.repositories.MovieRepository
@@ -16,6 +21,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
 import javax.inject.Singleton
 
 @Module
@@ -24,15 +33,35 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideMovieDataSource(): MovieDataSource = RemoteMovieDataSource()
+    fun provideApi(): ApiService {
+        val gson = GsonBuilder()
+            .registerTypeAdapter(LocalDate::class.java, LocalDateDeserializer())
+            .create()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(OkHttpClient.Builder().addInterceptor { chain ->
+                val request = chain.request().newBuilder().addHeader("Authorization", "Bearer $API_TOKEN").build()
+                chain.proceed(request)
+            }.build())
+            .build()
+            .create(ApiService::class.java)
+    }
 
     @Provides
     @Singleton
-    fun provideAuthDataSource(): AuthDataSource = RemoteAuthDataSource()
+    fun provideMovieDataSource(apiService: ApiService): MovieDataSource =
+        RemoteMovieDataSource(apiService)
 
     @Provides
     @Singleton
-    fun provideLogoutDataSource(): LogoutDataSource = RemoteLogoutDataSource()
+    fun provideAuthDataSource(apiService: ApiService): AuthDataSource =
+        RemoteAuthDataSource(apiService)
+
+    @Provides
+    @Singleton
+    fun provideLogoutDataSource(apiService: ApiService): LogoutDataSource =
+        RemoteLogoutDataSource(apiService)
 
     @Provides
     @Singleton
