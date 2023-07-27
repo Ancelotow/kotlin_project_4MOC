eb71541c3e034e47ae214cd1779ec90706456d8e
Ancelotow <ancelotow@gmail.com>
2023-07-27 17:54:48 +0200

    [US-6] Unit test to get new movies (Error & Loading)

--- app/src/test/java/com/oye/moviepedia/HomeViewModelTest.kt
+++ app/src/test/java/com/oye/moviepedia/HomeViewModelTest.kt
@@ -1,10 +1,10 @@
 package com.oye.moviepedia
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.viewModelScope
 import com.oye.moviepedia.domain.entities.Movie
 import com.oye.moviepedia.domain.interactors.HomeInteractor
-import com.oye.moviepedia.domain.uses_cases.NewMovieState
+import com.oye.moviepedia.domain.uses_cases.NewMovieError
+import com.oye.moviepedia.domain.uses_cases.NewMovieLoading
 import com.oye.moviepedia.domain.uses_cases.NewMovieSuccess
 import com.oye.moviepedia.domain.uses_cases.NewMovieUseCase
 import com.oye.moviepedia.domain.uses_cases.NowPlayingMovieUseCase
@@ -15,7 +15,6 @@ import com.oye.moviepedia.ui.home.HomeViewModel
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import org.junit.After
 import org.junit.Before
@@ -24,7 +23,6 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import kotlinx.coroutines.test.*
-import org.mockito.MockitoAnnotations
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class HomeViewModelTest {
@@ -58,14 +56,29 @@ class HomeViewModelTest {
         testDispatcher.cleanupTestCoroutines()
     }
 
-
     @Test
-    @ExperimentalCoroutinesApi
-    fun `test getNewMovies`() {
+    fun `test get New Movies with success`() {
         val newMovies = emptyList<Movie>()
         `when`(mockNewMovieUseCase.invoke()).thenReturn(flowOf(NewMovieSuccess(newMovies)))
         viewModel.onEventChanged(HomeEvent.OnNewMovies)
         val expectedState = NewMovieSuccess(newMovies)
+        assertEquals(viewModel.newMoviesState.value, expectedState)
+    }
+
+    @Test
+    fun `test get New Movies with error`() {
+        val exception = Exception("Test error")
+        `when`(mockNewMovieUseCase.invoke()).thenReturn(flowOf(NewMovieError(exception)))
+        viewModel.onEventChanged(HomeEvent.OnNewMovies)
+        val expectedState = NewMovieError(exception)
+        assertEquals(viewModel.newMoviesState.value, expectedState)
+    }
+
+    @Test
+    fun `test get New Movies loading`() {
+        `when`(mockNewMovieUseCase.invoke()).thenReturn(flowOf(NewMovieLoading))
+        viewModel.onEventChanged(HomeEvent.OnNewMovies)
+        val expectedState = NewMovieLoading
         assertEquals(viewModel.newMoviesState.value, expectedState)
     }
 }
