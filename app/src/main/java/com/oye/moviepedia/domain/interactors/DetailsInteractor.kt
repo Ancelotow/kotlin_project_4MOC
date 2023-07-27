0ae69e0c518d7b3b91575a8b0b0087120c53c5a6
Ancelotow <ancelotow@gmail.com>
2023-07-27 20:44:11 +0200

    Add dynamic link to share movie

--- app/src/main/java/com/oye/moviepedia/domain/interactors/DetailsInteractor.kt
+++ app/src/main/java/com/oye/moviepedia/domain/interactors/DetailsInteractor.kt
@@ -1,8 +1,10 @@
 package com.oye.moviepedia.domain.interactors
 
+import com.oye.moviepedia.domain.uses_cases.GetMovieDynamicLinkUseCase
 import com.oye.moviepedia.domain.uses_cases.MovieDetailsUseCase
 import javax.inject.Inject
 
 data class DetailsInteractor @Inject constructor(
-    val movieDetailsUseCase: MovieDetailsUseCase
+    val movieDetailsUseCase: MovieDetailsUseCase,
+    val getMovieDynamicLinkUseCase: GetMovieDynamicLinkUseCase
 )
--- app/src/main/java/com/oye/moviepedia/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/oye/moviepedia/ui/details/DetailsViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.oye.moviepedia.domain.entities.Movie
+import com.oye.moviepedia.domain.entities.MovieDetails
 import com.oye.moviepedia.domain.interactors.DetailsInteractor
+import com.oye.moviepedia.domain.uses_cases.GetMovieDynamicLinkState
 import com.oye.moviepedia.domain.uses_cases.MovieDetailsError
 import com.oye.moviepedia.domain.uses_cases.MovieDetailsState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,10 +21,16 @@ class DetailsViewModel @Inject constructor(
     private val _movieDetails = MutableLiveData<MovieDetailsState>()
     val movieDetails: LiveData<MovieDetailsState> = _movieDetails
 
+    private val _dynamicLink = MutableLiveData<GetMovieDynamicLinkState>()
+    val dynamicLink: LiveData<GetMovieDynamicLinkState> = _dynamicLink
+
     fun onEventChanged(event: DetailsScreenEvent) {
         when (event) {
             is DetailsScreenEvent.OnGetMovie -> {
                 getMovie(event.movieId)
+            }
+            is DetailsScreenEvent.OnGetDynamicLink -> {
+                getDynamicLink(event.movie)
             }
         }
     }
@@ -30,6 +39,14 @@ class DetailsViewModel @Inject constructor(
         viewModelScope.launch {
             detailsInteractor.movieDetailsUseCase.getMovie(id).collect {
                 _movieDetails.value = it
+            }
+        }
+    }
+
+    private fun getDynamicLink(movie: MovieDetails) {
+        viewModelScope.launch {
+            detailsInteractor.getMovieDynamicLinkUseCase.invoke(movie).collect {
+                _dynamicLink.value = it
             }
         }
     }
