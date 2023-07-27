f4645a5dfffdc0eb7510cf93f1acba5b89614fd8
Ancelotow <ancelotow@gmail.com>
2023-07-27 23:33:50 +0200

    Add crashlytics with Firebase

--- app/src/main/java/com/oye/moviepedia/MoviepediaApplication.kt
+++ app/src/main/java/com/oye/moviepedia/MoviepediaApplication.kt
@@ -2,6 +2,7 @@ package com.oye.moviepedia
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,6 +11,11 @@ class MoviepediaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        setupCrashlytics()
+    }
+
+    private fun setupCrashlytics() {
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
     }
 
 }
